8d0a54f5abb6cfb1673eb69f8624deb185281c56
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-04-09 17:49:31 -0500

    "coverMeInPiss" -Psych Lua Function 2022

---
+++ assets/preload/stages/EndlessLoop.lua
@@ -0,0 +1,8 @@
+function onCreate()
+	makeLuaSprite('street', 'funkinAVI/phase1/street/Mickeybg', -600, -170);
+	addLuaSprite('street');
+	addEffect1('street', 0.003);
+	addEffect4('street');
+end
+
+--fuck you source

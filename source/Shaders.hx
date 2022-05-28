9dfc04657c3b505d0bd7c208e060afcc7acc459c
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-05-28 17:37:29 -0500

    Update Shaders.hx

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -95,85 +95,124 @@ class ChromaticAberrationEffect extends Effect
 
 }
 
-class BetterVHSShader extends FlxShader
+// Shader in question: https://www.shadertoy.com/view/ldjGzV
+// Original haxefl code: https://github.com/jobf/haxeflixel-vcr-effect-shader/blob/master/source/VhsShader.hx
+// Modified by zackdroid so it can be much easier to handle.
+
+class VhsHandler
 {
-	@:glFragmentSource('
-		#pragma header
-			float noise(vec2 p)
-		{
-			float s = texture(iChannel1,vec2(1.,2.*cos(iTime))*iTime*8. + p*1.).x;
-			s *= s;
-			return s;
-		}
+    public var shader:VhsShader;
+    public var noise(default, set):Float = 0.0;
+    public var intensity(default,set):Float = 0.2;
 
-		float onOff(float a, float b, float c)
-		{
-			return step(c, sin(iTime + a*cos(iTime*b)));
-		}
-
-		float ramp(float y, float start, float end)
-		{
-			float inside = step(start,y) - step(end,y);
-			float fact = (y-start)/(end-start)*inside;
-			return (1.-fact) * inside;
-
-		}
-
-		float stripes(vec2 uv)
-		{
-
-			float noi = noise(uv*vec2(0.5,1.) + vec2(1.,3.));
-			return ramp(mod(uv.y*4. + iTime/2.+sin(iTime + sin(iTime*0.63)),1.),0.5,0.6)*noi;
-		}
-
-		vec3 getVideo(vec2 uv)
-		{
-			vec2 look = uv;
-			float window = 1./(1.+20.*(look.y-mod(iTime/4.,1.))*(look.y-mod(iTime/4.,1.)));
-			look.x = look.x + sin(look.y*10. + iTime)/50.*onOff(4.,4.,.3)*(1.+cos(iTime*80.))*window;
-			float vShift = 0.4*onOff(2.,3.,.9)*(sin(iTime)*sin(iTime*20.) + 
-												 (0.5 + 0.1*sin(iTime*200.)*cos(iTime)));
-			look.y = mod(look.y + vShift, 1.);
-			vec3 video = vec3(texture(iChannel0,look));
-			return video;
-		}
-
-		vec2 screenDistort(vec2 uv)
-		{
-			uv -= vec2(.5,.5);
-			uv = uv*1.2*(1./1.2+2.*uv.x*uv.x*uv.y*uv.y);
-			uv += vec2(.5,.5);
-			return uv;
-		}
-
-		void mainImage( out vec4 fragColor, in vec2 fragCoord )
-		{
-			vec2 uv = fragCoord.xy / iResolution.xy;
-			uv = screenDistort(uv);
-			vec3 video = getVideo(uv);
-			float vigAmt = 3.+.3*sin(iTime + 5.*cos(iTime*5.));
-			float vignette = (1.-vigAmt*(uv.y-.5)*(uv.y-.5))*(1.-vigAmt*(uv.x-.5)*(uv.x-.5));
-
-			video += stripes(uv);
-			video += noise(uv*2.)/2.;
-			video *= vignette;
-			video *= (12.+mod(uv.y*30.+iTime,1.))/13.;
-
-			fragColor = vec4(video,1.0);
-		}')
-		public function new()
+	public function new()
 	{
-		super();
+		//super();
+        shader = new VhsShader();
+    	shader.iTime.value = [0.0];
+	    shader.noisePercent.value = [0.0];
+        shader.intensity.value = [0.2];
+	}
+
+	public function update(elapsed:Float)
+	{
+    	shader.iTime.value[0] += elapsed;
+	}
+
+	function set_noise(value:Float):Float {
+	    shader.noisePercent.value = [value];
+        noise = value;
+        return value;
+	}
+	function set_intensity(value:Float):Float {
+    	shader.intensity.value = [value];
+        intensity = value;
+        return value;
 	}
 }
 
-class BetterVHSEffect extends Effect{
-	
-	public var shader:BetterVHSShader = new BetterVHSShader();
-	
-	public function new(){
-		
-	}
+class VhsShader extends FlxShader {
+    @:glFragmentSource('
+    #pragma header
+    
+    uniform float iTime;
+    uniform sampler2D noiseTexture;
+    uniform float noisePercent;
+    uniform float intensity;
+    
+    float rand(vec2 co)
+    {
+        //no highp, crashes bullshit.
+        float a = 12.9898;
+        float b = 78.233;
+        float c = 43758.5453;
+        float dt= dot(co.xy ,vec2(a,b));
+        float sn= mod(dt,3.14);
+        return fract(sin(sn) * c);
+    }
+        
+    float noise(vec2 p)
+    {
+        return rand(p) * noisePercent;
+    }
+    
+    float onOff(float a, float b, float c)
+    {
+        return step(c, sin(iTime + a*cos(iTime*b)));
+    }
+
+    float ramp(float y, float start, float end)
+    {
+        float inside = step(start,y) - step(end,y);
+        float fact = (y-start)/(end-start)*inside;
+        return (1.-fact) * inside;
+    }
+
+    float stripes(vec2 uv)
+    {
+        float noi = noise(uv*vec2(0.5,1.) + vec2(1.,3.));
+        return ramp(mod(uv.y*4. + iTime/2.+sin(iTime + sin(iTime*0.63)),1.),0.5,0.6)*noi;
+    }
+
+    vec4 getVideo(vec2 uv)
+    {
+        vec2 look = uv;
+        float window = 1./(1.+20.*(look.y-mod(iTime/4.,1.))*(look.y-mod(iTime/4.,1.)));
+        look.x = look.x + sin(look.y*10. + iTime)/50.*onOff(4.,4.,.3)*(1.+cos(iTime*80.))*window;
+        float vShift = 0.4*onOff(2.,3.,.9) * (sin(iTime)*sin(iTime*20.) + (0.5 + 0.1*sin(iTime*200.)*cos(iTime)));
+        look.y = mod(look.y + vShift, 1.);
+        vec4 video = vec4(flixel_texture2D(bitmap,mix(uv,look,intensity)));
+        return video;
+    }
+
+    vec2 screenDistort(vec2 uv)
+    {
+        uv -= vec2(.5,.5);
+        uv = uv*1.2*(1./1.2+2.*uv.x*uv.x*uv.y*uv.y);
+        uv += vec2(.5,.5);
+        return uv;
+    }
+
+    void main()
+    {
+        vec2 uv = openfl_TextureCoordv.xy;
+        uv = screenDistort(uv);
+        vec4 video = getVideo(uv);
+        float daAlp = video.a; // we dont want a black camera with the efx on it.
+        float vigAmt = 3.+.3*sin(iTime + 5.*cos(iTime*5.));
+        float vignette = (1.-vigAmt*(uv.y-.5)*(uv.y-.5))*(1.-vigAmt*(uv.x-.5)*(uv.x-.5));
+        
+        video += stripes(uv);
+        video += noise(uv*2.)/2.;
+        video *= vignette;
+        video *= (12.+mod(uv.y*30.+iTime,1.))/13.;
+        
+        gl_FragColor = vec4(video.rgb,daAlp);
+    }
+    ')
+    public function new() {
+        super();
+    }
 }
 
 class ScanlineEffect extends Effect
@@ -186,324 +225,6 @@ class ScanlineEffect extends Effect
 	}
 	
 	
-}
-
-class TVDistortShader extends FlxShader
-{
-	@:glFragmentSource('
-		#pragma header
-			// change these values to 0.0 to turn off individual effects
-			float vertJerkOpt = 1.0;
-			float vertMovementOpt = 1.0;
-			float bottomStaticOpt = 1.0;
-			float scalinesOpt = 1.0;
-			float rgbOffsetOpt = 1.0;
-			float horzFuzzOpt = 1.0;
-
-			// Noise generation functions borrowed from: 
-			// https://github.com/ashima/webgl-noise/blob/master/src/noise2D.glsl
-
-			vec3 mod289(vec3 x) {
-			  return x - floor(x * (1.0 / 289.0)) * 289.0;
-			}
-
-			vec2 mod289(vec2 x) {
-			  return x - floor(x * (1.0 / 289.0)) * 289.0;
-			}
-
-			vec3 permute(vec3 x) {
-			  return mod289(((x*34.0)+1.0)*x);
-			}
-
-			float snoise(vec2 v)
-			  {
-			  const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
-					      0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
-					     -0.577350269189626,  // -1.0 + 2.0 * C.x
-					      0.024390243902439); // 1.0 / 41.0
-			// First corner
-			  vec2 i  = floor(v + dot(v, C.yy) );
-			  vec2 x0 = v -   i + dot(i, C.xx);
-
-			// Other corners
-			  vec2 i1;
-			  //i1.x = step( x0.y, x0.x ); // x0.x > x0.y ? 1.0 : 0.0
-			  //i1.y = 1.0 - i1.x;
-			  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
-			  // x0 = x0 - 0.0 + 0.0 * C.xx ;
-			  // x1 = x0 - i1 + 1.0 * C.xx ;
-			  // x2 = x0 - 1.0 + 2.0 * C.xx ;
-			  vec4 x12 = x0.xyxy + C.xxzz;
-			  x12.xy -= i1;
-
-			// Permutations
-			  i = mod289(i); // Avoid truncation effects in permutation
-			  vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
-					+ i.x + vec3(0.0, i1.x, 1.0 ));
-
-			  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
-			  m = m*m ;
-			  m = m*m ;
-
-			// Gradients: 41 points uniformly over a line, mapped onto a diamond.
-			// The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
-
-			  vec3 x = 2.0 * fract(p * C.www) - 1.0;
-			  vec3 h = abs(x) - 0.5;
-			  vec3 ox = floor(x + 0.5);
-			  vec3 a0 = x - ox;
-
-			// Normalise gradients implicitly by scaling m
-			// Approximation of: m *= inversesqrt( a0*a0 + h*h );
-			  m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
-
-			// Compute final noise value at P
-			  vec3 g;
-			  g.x  = a0.x  * x0.x  + h.x  * x0.y;
-			  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
-			  return 130.0 * dot(m, g);
-			}
-
-			float staticV(vec2 uv) {
-			    float staticHeight = snoise(vec2(9.0,iTime*1.2+3.0))*0.3+5.0;
-			    float staticAmount = snoise(vec2(1.0,iTime*1.2-6.0))*0.1+0.3;
-			    float staticStrength = snoise(vec2(-9.75,iTime*0.6-3.0))*2.0+2.0;
-				return (1.0-step(snoise(vec2(5.0*pow(iTime,2.0)+pow(uv.x*7.0,1.2),pow((mod(iTime,100.0)+100.0)*uv.y*0.3+3.0,staticHeight))),staticAmount))*staticStrength;
-			}
-
-
-			void mainImage( out vec4 fragColor, in vec2 fragCoord )
-			{
-
-				vec2 uv =  fragCoord.xy/iResolution.xy;
-
-				float jerkOffset = (1.0-step(snoise(vec2(iTime*1.3,5.0)),0.8))*0.05;
-
-				float fuzzOffset = snoise(vec2(iTime*15.0,uv.y*80.0))*0.003;
-				float largeFuzzOffset = snoise(vec2(iTime*1.0,uv.y*25.0))*0.004;
-
-			    float vertMovementOn = (1.0-step(snoise(vec2(iTime*0.2,8.0)),0.4))*vertMovementOpt;
-			    float vertJerk = (1.0-step(snoise(vec2(iTime*1.5,5.0)),0.6))*vertJerkOpt;
-			    float vertJerk2 = (1.0-step(snoise(vec2(iTime*5.5,5.0)),0.2))*vertJerkOpt;
-			    float yOffset = abs(sin(iTime)*4.0)*vertMovementOn+vertJerk*vertJerk2*0.3;
-			    float y = mod(uv.y+yOffset,1.0);
-
-
-				float xOffset = (fuzzOffset + largeFuzzOffset) * horzFuzzOpt;
-
-			    float staticVal = 0.0;
-
-			    for (float y = -1.0; y <= 1.0; y += 1.0) {
-				float maxDist = 5.0/200.0;
-				float dist = y/200.0;
-				staticVal += staticV(vec2(uv.x,uv.y+dist))*(maxDist-abs(dist))*1.5;
-			    }
-
-			    staticVal *= bottomStaticOpt;
-
-				float red 	=   texture(	iChannel0, 	vec2(uv.x + xOffset -0.01*rgbOffsetOpt,y)).r+staticVal;
-				float green = 	texture(	iChannel0, 	vec2(uv.x + xOffset,	  y)).g+staticVal;
-				float blue 	=	texture(	iChannel0, 	vec2(uv.x + xOffset +0.01*rgbOffsetOpt,y)).b+staticVal;
-
-				vec3 color = vec3(red,green,blue);
-				float scanline = sin(uv.y*800.0)*0.04*scalinesOpt;
-				color -= scanline;
-
-				fragColor = vec4(color,1.0);
-			}')
-		public function new()
-	{
-		super();
-	}
-}
-
-class TVDistortEffect extends Effect{
-	
-	public var shader:TVDistortShader = new TVDistortShader();
-	
-	public function new(){
-		
-	}
-}
-
-class TapeOverlayShader extends FlxShader
-{
-	@:glFragmentSource('
-		#pragma header
-			const float range = 0.05;
-		const float noiseQuality = 250.0;
-		const float noiseIntensity = 0.0088;
-		const float offsetIntensity = 0.02;
-		const float colorOffsetIntensity = 1.3;
-
-		float rand(vec2 co)
-		{
-		    return fract(sin(dot(co.xy ,vec2(12.9898,78.233))) * 43758.5453);
-		}
-
-		float verticalBar(float pos, float uvY, float offset)
-		{
-		    float edge0 = (pos - range);
-		    float edge1 = (pos + range);
-
-		    float x = smoothstep(edge0, pos, uvY) * offset;
-		    x -= smoothstep(pos, edge1, uvY) * offset;
-		    return x;
-		}
-
-		void mainImage( out vec4 fragColor, in vec2 fragCoord )
-		{
-			vec2 uv = fragCoord.xy / iResolution.xy;
-
-		    for (float i = 0.0; i < 0.71; i += 0.1313)
-		    {
-			float d = mod(iTime * i, 1.7);
-			float o = sin(1.0 - tan(iTime * 0.24 * i));
-			o *= offsetIntensity;
-			uv.x += verticalBar(d, uv.y, o);
-		    }
-
-		    float uvY = uv.y;
-		    uvY *= noiseQuality;
-		    uvY = float(int(uvY)) * (1.0 / noiseQuality);
-		    float noise = rand(vec2(iTime * 0.00001, uvY));
-		    uv.x += noise * noiseIntensity;
-
-		    vec2 offsetR = vec2(0.006 * sin(iTime), 0.0) * colorOffsetIntensity;
-		    vec2 offsetG = vec2(0.0073 * (cos(iTime * 0.97)), 0.0) * colorOffsetIntensity;
-
-		    float r = texture(iChannel0, uv + offsetR).r;
-		    float g = texture(iChannel0, uv + offsetG).g;
-		    float b = texture(iChannel0, uv).b;
-
-		    vec4 tex = vec4(r, g, b, 1.0);
-		    fragColor = tex;
-		}')
-		public function new()
-	{
-		super();
-	}
-}
-
-class TapeOverlayEffect extends Effect{
-	
-	public var shader:TapeOverlayShader = new TapeOverlayShader();
-	
-	public function new(){
-		
-	}
-}
-	
-class FilmScratchShader extends FlxShader
-{
-	@:glFragmentSource('
-		#pragma header
-			precision lowp float;
-			const float ScratchValue = 0.31;
-
-			vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-			vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
-			vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
-			float snoise (vec2 v)
-			{
-				const vec4 C = vec4(0.211324865405187,	// (3.0-sqrt(3.0))/6.0
-							0.366025403784439,	// 0.5*(sqrt(3.0)-1.0)
-							-0.577350269189626,	// -1.0 + 2.0 * C.x
-							0.024390243902439);	// 1.0 / 41.0
-
-				// First corner
-				vec2 i  = floor(v + dot(v, C.yy) );
-				vec2 x0 = v -   i + dot(i, C.xx);
-
-				// Other corners
-				vec2 i1;
-				i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
-				vec4 x12 = x0.xyxy + C.xxzz;
-				x12.xy -= i1;
-
-				// Permutations
-				i = mod289(i); // Avoid truncation effects in permutation
-				vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
-					+ i.x + vec3(0.0, i1.x, 1.0 ));
-
-				vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
-				m = m*m ;
-				m = m*m ;
-
-				// Gradients: 41 points uniformly over a line, mapped onto a diamond.
-				// The ring size 17*17 = 289 is close to a multiple of 41 (41*7 = 287)
-
-				vec3 x = 2.0 * fract(p * C.www) - 1.0;
-				vec3 h = abs(x) - 0.5;
-				vec3 ox = floor(x + 0.5);
-				vec3 a0 = x - ox;
-
-				// Normalise gradients implicitly by scaling m
-				// Approximation of: m *= inversesqrt( a0*a0 + h*h );
-				m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
-
-				// Compute final noise value at P
-				vec3 g;
-				g.x  = a0.x  * x0.x  + h.x  * x0.y;
-				g.yz = a0.yz * x12.xz + h.yz * x12.yw;
-				return 130.0 * dot(m, g);
-			}
-
-			//note: [0;1]
-			float rand( vec2 n ) {
-			    return fract(sin(dot(n.xy, vec2(12.9898, 78.233)))* 43758.5453);
-			}
-
-			//note: [-1;1]
-			float srand( vec2 n ) {
-			    return rand(n) * 2.0 - 1.0;
-			}
-
-
-			void mainImage( out vec4 fragColor, in vec2 fragCoord )
-			{
-			    // Normalized pixel coordinates (from 0 to 1)
-			    vec2 uv = fragCoord/iResolution.xy;
-			    vec3 finalColour = texture(iChannel0, uv).xyz;
-			    float RandomValue = rand(uv);
-			    float TimeLapse = floor(iTime*1000./50.);
-
-			    if ( RandomValue < ScratchValue )
-				{
-					// Pick a random spot to show scratches
-					float dist = 1.0 / ScratchValue;
-					float d = distance(uv, vec2(RandomValue * dist, RandomValue * dist));
-					if ( d < 0.4)
-					{
-						// Generate the scratch
-						float xPeriod = 8.0;
-						float yPeriod = 1.;
-						float pi = 3.141592;
-						float phase = TimeLapse;
-						float turbulence = snoise(uv * 2.5);
-						float vScratch = 0.5 + (sin(((uv.x * xPeriod + uv.y * yPeriod + turbulence)) * pi + phase) * 0.5);
-						vScratch = clamp((vScratch * 10000.0) + 0.5, 0.0, 1.0);
-
-						finalColour.xyz /= vScratch;
-					}
-				}
-
-			    fragColor = vec4(finalColour, 1.0);
-
-			}')
-		public function new()
-	{
-		super();
-	}
-}
-
-class FilmScratchEffect extends Effect{
-	
-	public var shader:FilmScratchShader = new FilmScratchShader();
-	
-	public function new(){
-		
-	}
 }
 
 class Scanline extends FlxShader

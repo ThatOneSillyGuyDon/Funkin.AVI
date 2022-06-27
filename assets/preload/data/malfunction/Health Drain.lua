d9aeeab38d8a34d761425e010c9f8bc90f2dafba
Matt 3 <ronherelol@gmail.com>
2022-06-27 00:06:13 -0500

    small fix with this, ig it makes it fair for all??????

--- assets/preload/data/malfunction/Health Drain.lua
+++ assets/preload/data/malfunction/Health Drain.lua
@@ -1,20 +1,28 @@
 function onUpdate()
     if getPropertyFromClass('ClientPrefs', 'events') then
     function opponentNoteHit(id,data,type,sus)
-        getPropertyFromClass("openfl.Lib", "application.window.x")
-        getPropertyFromClass("openfl.Lib", "application.window.y")
         triggerEvent('Screen Shake','0.1, 0.002','0.1, 0.002')
         addEffect4('game', 0.1, 0);
         addEffect4('hud', 0.1, 0);
         addEffect1('hud', 0.01);
         addEffect1('game', 0.01);
         runTimer('chromaticTimer', 0.04, 1);
+        if difficulty == 0 then
         health = getProperty('health')
         if getProperty('health') > 0.08 then
             setProperty('health', health- 0.009); --doing fair
         end
-        setPropertyFromClass('openfl.Lib', 'application.window.y', getRandomFloat(0, 313))
-        setPropertyFromClass('openfl.Lib', 'application.window.x', getRandomFloat(0, 313))
+    end
+        if difficulty == 1 then
+            getPropertyFromClass("openfl.Lib", "application.window.x")
+            getPropertyFromClass("openfl.Lib", "application.window.y")
+            health = getProperty('health')
+            if getProperty('health') > 0.08 then
+                setProperty('health', health- 0.09); --die bitch
+            end
+            setPropertyFromClass('openfl.Lib', 'application.window.y', getRandomFloat(0, 313))
+            setPropertyFromClass('openfl.Lib', 'application.window.x', getRandomFloat(0, 313))
+        end
     end
     
     function onTimerCompleted(tag)
--- assets/preload/data/malfunction/the fact i have to put the window movement in a whole other lua file.lua
+++ assets/preload/data/malfunction/the fact i have to put the window movement in a whole other lua file.lua
@@ -1,8 +1,4 @@
 --https://gamebanana.com/tuts/15095 For Window Shit
-local WindowDanceX = false;
-local WindowDanceY = false;
-local WindowDanceXY = false;
-local WindowRandom = false;
 local SineElap = 0;
 
 function onCreate()
@@ -10,31 +6,18 @@ function onCreate()
     windowx = getPropertyFromClass("openfl.Lib", "application.window.x")
 end
 
-function onStepHit()
-    if curStep >= 32 and curStep <= 95 then
-        WindowDanceX = true
-    end
-    if curStep == 95 + 1 then
-        WindowDanceX = false
-    end
-    if curStep >= 96 and curStep <= 159 then
-        WindowDanceY = true
-    end
-    if curStep == 159 + 1 then
-        WindowDanceY = false
-    end
-end
-
 function onUpdatePost(elapsed)
     SineElap = SineElap + (elapsed * 3)
-    if WindowDanceX == true then
+    if difficulty == 1 then
+        songPos = getSongPosition()
+        local currentBeat = (songPos/1000)*(bpm/60)
+        setPropertyFromClass('openfl.Lib', 'application.window.x', 3000*math.cos(SineElap)/10+windowx)
+        setPropertyFromClass('openfl.Lib', 'application.window.y', 3000*math.sin(SineElap)/10+windowy)
+        setProperty('window.width', windowW + math.sin(currentBeat) * 30)--height
+        setProperty('window.height', windowH + math.sin(currentBeat) * 30)--height
+    end
+    if difficulty == 0 then
         setPropertyFromClass('openfl.Lib', 'application.window.x', 1000*math.cos(SineElap)/10+windowx)
-    end
-    if WindowDanceY == true then
-        setPropertyFromClass('openfl.Lib', 'application.window.y', 1000*math.sin(SineElap)/10+windowy)
-    end
-    if WindowDanceXY == true then
-        setPropertyFromClass('openfl.Lib', 'application.window.y', 1000*math.cos(SineElap)/10+windowx)
         setPropertyFromClass('openfl.Lib', 'application.window.y', 1000*math.sin(SineElap)/10+windowy)
     end
 end

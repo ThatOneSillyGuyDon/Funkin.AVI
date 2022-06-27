a777a0ba925d8abd3a94dcbddfb070237a6ca2a8
Matt 3 <ronherelol@gmail.com>
2022-06-27 00:07:32 -0500

    ads

--- assets/preload/data/malfunction/the fact i have to put the window movement in a whole other lua file.lua
+++ assets/preload/data/malfunction/the fact i have to put the window movement in a whole other lua file.lua
@@ -4,6 +4,8 @@ local SineElap = 0;
 function onCreate()
     windowy = getPropertyFromClass("openfl.Lib", "application.window.y")
     windowx = getPropertyFromClass("openfl.Lib", "application.window.x")
+    getPropertyFromClass("openfl.Lib", "application.window.x")
+    getPropertyFromClass("openfl.Lib", "application.window.y")
 end
 
 function onUpdatePost(elapsed)
@@ -13,8 +15,10 @@ function onUpdatePost(elapsed)
         local currentBeat = (songPos/1000)*(bpm/60)
         setPropertyFromClass('openfl.Lib', 'application.window.x', 3000*math.cos(SineElap)/10+windowx)
         setPropertyFromClass('openfl.Lib', 'application.window.y', 3000*math.sin(SineElap)/10+windowy)
-        setProperty('window.width', windowW + math.sin(currentBeat) * 30)--height
+        setProperty('window.width', windowW + math.sin(currentBeat) * 30)--width
         setProperty('window.height', windowH + math.sin(currentBeat) * 30)--height
+        setPropertyFromClass('openfl.Lib', 'application.window.y', getRandomFloat(80, 283))
+        setPropertyFromClass('openfl.Lib', 'application.window.x', getRandomFloat(80, 283))
     end
     if difficulty == 0 then
         setPropertyFromClass('openfl.Lib', 'application.window.x', 1000*math.cos(SineElap)/10+windowx)

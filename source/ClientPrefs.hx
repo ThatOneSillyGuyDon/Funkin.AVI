b28253c346dae069ae0a56f54f56c4cbe71b1aaf
Jsa010 <98182734+Jsa010@users.noreply.github.com>
2022-07-14 11:36:51 -0300

    :D
    joe, mama

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -7,6 +7,7 @@ import flixel.graphics.FlxGraphic;
 import Controls;
 
 class ClientPrefs /*Client Preferetions*/ {
+	public static var noVsMouseSob:Bool = false;
 	public static var simpleMenu:Bool = false;
 	public static var cutscenes:Bool = false;
 	public static var FPStext:Bool = false;
@@ -126,6 +127,7 @@ class ClientPrefs /*Client Preferetions*/ {
 	}
 
 	public static function saveSettings() {
+		FlxG.save.data.noVsMouseSob = noVsMouseSob;
 		FlxG.save.data.simpleMenu = simpleMenu;
 		FlxG.save.data.cutscens = cutscenes;
 		FlxG.save.data.FPStext = FPStext;
@@ -198,6 +200,9 @@ class ClientPrefs /*Client Preferetions*/ {
 	}
 
 	public static function loadPrefs() {
+		if(FlxG.save.data.noVsMouseSob != null) {
+			noVsMouseSob = FlxG.save.data.noVsMouseSob;
+		}
 		if(FlxG.save.data.simpleMenu != null) {
 			simpleMenu = FlxG.save.data.simpleMenu;
 		}
--- source/StrumNote.hx
+++ source/StrumNote.hx
@@ -189,8 +189,7 @@ class StrumNote extends FlxSprite
 				resetAnim = 0;
 			}
 		}
-		//if(animation.curAnim != null){ //my bad i was upset
-		if(animation.curAnim.name == 'confirm' && !PlayState.isPixelStage) {
+		if((animation.curAnim != null && animation.curAnim.name == 'confirm') && !PlayState.isPixelStage) {
 			centerOrigin();
 		//}
 		}

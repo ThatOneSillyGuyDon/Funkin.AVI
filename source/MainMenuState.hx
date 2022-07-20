4554a4defe12802bfc6903929b13e8684a9f5fa7
Jsa010 <98182734+Jsa010@users.noreply.github.com>
2022-07-20 16:46:41 -0300

    no more achievements

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -202,7 +202,7 @@ class MainMenuState extends MusicBeatState
 			else
 				menuItem.y = 108 + (0 * 90);
 
-			// Achievement's
+			// Credits
 			var menuItem:FlxSprite = new FlxSprite(700, 400);
 			menuItem.scale.x = scale;
 			menuItem.scale.y = scale;
@@ -231,11 +231,11 @@ class MainMenuState extends MusicBeatState
 			else
 				menuItem.y = 108 + (0 * 90);
 
-			// Credits
-			var menuItem:FlxSprite = new FlxSprite(700, 700);
+			// Options
+			var menuItem:FlxSprite = new FlxSprite(700, 600);
 			menuItem.scale.x = scale;
 			menuItem.scale.y = scale;
-			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[2]);
+			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[3]);
 			menuItem.animation.addByPrefix('idle', optionShit[3] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionShit[3] + " white", 24);
 			menuItem.animation.play('idle');
@@ -260,34 +260,6 @@ class MainMenuState extends MusicBeatState
 			else
 				menuItem.y = 108 + (0 * 90);
 
-			// Settings
-			var menuItem:FlxSprite = new FlxSprite(700, 900);
-			menuItem.scale.x = scale;
-			menuItem.scale.y = scale;
-			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[3]);
-			menuItem.animation.addByPrefix('idle', optionShit[4] + " basic", 24);
-			menuItem.animation.addByPrefix('selected', optionShit[4] + " white", 24);
-			menuItem.animation.play('idle');
-			menuItem.ID = 4;
-			//menuItem.screenCenter(X);
-			menuItems.add(menuItem);
-			var scr:Float = (optionShit.length - 2) * 0.135;
-			if(optionShit.length < 6) scr = 4;
-			menuItem.scrollFactor.set(0, scr);
-			menuItem.antialiasing = ClientPrefs.globalAntialiasing;
-			//menuItem.setGraphicSize(Std.int(menuItem.width * 0.58));
-			menuItem.updateHitbox();
-			if (firstStart)
-				FlxTween.tween(menuItem, {y: 100 + (0 * 90)}, 1 + (0 * 0.25), {
-					ease: FlxEase.elasticInOut,
-					onComplete: function(flxTween:FlxTween)
-					{
-						finishedFunnyMove = true;
-						changeItem();
-					}
-				});
-			else
-				menuItem.y = 108 + (0 * 90);
 		
 			// Donate
 			/*var menuItem:FlxSprite = new FlxSprite(100, 700);

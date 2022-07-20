c460f18ae7aa10f1111780c128b9566e7b62b630
Jsa010 <98182734+Jsa010@users.noreply.github.com>
2022-07-20 16:27:44 -0300

    modified:   source/MainMenuState.hx modified:   source/MainMenuState.hx modified:   source/MainMenuState.hx modified:   source/MainMenuState.hx modified:   source/MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -45,7 +45,7 @@ class MainMenuState extends MusicBeatState
 		'story_mode',
 		'freeplay',
 		//#if MODS_ALLOWED 'mods', #end
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
+		//#if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
 		//#if !switch 'donate', #end
 		'options'

14a3414fdd5246976bb4d20475b0ec1ea3be1aaa
Jsa010 <98182734+Jsa010@users.noreply.github.com>
2022-08-05 11:15:30 -0300

    i forgor, now i hardcode bless

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -408,7 +408,7 @@ if (!transitioning && skippedIntro)
 			if(titleText != null) titleText.animation.play('press');
 
 			FlxG.camera.flash(FlxColor.WHITE, 1);
-			FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
+			FlxG.sound.play(Paths.sound('funkinAVI/menu/select_sfx'), 0.7);
 
 			transitioning = true;
 			// FlxG.sound.music.stop();

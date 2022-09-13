6f1b835327bc2b1efdd17b9fb5fd6b4f685a4b6a
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-09-12 21:28:14 -0500

    man.

--- WindowAPI.hx
+++ WindowAPI.hx
@@ -1,19 +1,5 @@
 package;
 
-@:buildXml('
-<target id="haxe">
-    <lib name="dwmapi.lib" if="windows" />
-</target>
-')
-@:headerCode('
-#include <Windows.h>
-#include <cstdio>
-#include <iostream>
-#include <tchar.h>
-#include <dwmapi.h>
-#include <winuser.h>
-')
-
 @:cppFileCode('#include <windows.h>\n#include <dwmapi.h>\n\n#pragma comment(lib, "Dwmapi")')
 class CppAPI
 {
@@ -65,72 +51,4 @@ class WINDOW
 	{
 		return res;
 	}
-	
-	@:functionCode('
-        int darkMode = mode;
-        HWND window = GetActiveWindow();
-        if (S_OK != DwmSetWindowAttribute(window, 19, &darkMode, sizeof(darkMode))) {
-            DwmSetWindowAttribute(window, 20, &darkMode, sizeof(darkMode));
-        }
-        UpdateWindow(window);
-    ')
-	@:noCompletion
-	public static function _setWindowColorMode(mode:Int)
-	{
-	}
-
-	public static function setWindowColorMode(mode:WindowColorMode)
-	{
-		var darkMode:Int = cast(mode, Int);
-
-		if (darkMode > 1 || darkMode < 0)
-		{
-			trace("WindowColorMode Not Found...");
-
-			return;
-		}
-
-		_setWindowColorMode(darkMode);
-	}
-	
-	@:functionCode('
-	HWND window = GetActiveWindow();
-	SetWindowLong(window, GWL_EXSTYLE, GetWindowLong(window, GWL_EXSTYLE) ^ WS_EX_LAYERED);
-	')
-	@:noCompletion
-	public static function _setWindowLayered()
-	{
-	}
-
-	@:functionCode('
-        HWND window = GetActiveWindow();
-
-		float a = alpha;
-
-		if (alpha > 1) {
-			a = 1;
-		} 
-		if (alpha < 0) {
-			a = 0;
-		}
-
-       	SetLayeredWindowAttributes(window, 0, (255 * (a * 100)) / 100, LWA_ALPHA);
-
-    ')
-	/**
-	 * Set Whole Window's Opacity
-	 * ! MAKE SURE TO CALL CppAPI._setWindowLayered(); BEFORE RUNNING THIS
-	 * @param alpha 
-	 */
-	public static function setWindowAlpha(alpha:Float)
-	{
-		return alpha;
-	}
-
-}
-
-@:enum abstract WindowColorMode(Int)
-{
-	var DARK:WindowColorMode = 1;
-	var LIGHT:WindowColorMode = 0;
 }

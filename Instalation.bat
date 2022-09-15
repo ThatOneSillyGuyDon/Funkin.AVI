b3d46facb450fd5e02ad6b01e9e3bedce397c807
Jsa010 <98182734+Jsa010@users.noreply.github.com>
2022-09-14 22:02:18 -0300

    added missing things

--- Instalation.bat
+++ Instalation.bat
@@ -10,6 +10,9 @@ haxelib install flixel
 haxelib install flixel-addons
 haxelib install flixel-ui
 haxelib install hscript
+haxelib git tentools https://github.com/TentaRJ/tentools.git
+haxelib git systools https://github.com/haya3218/systools
+haxelib run lime rebuild systools windows
 haxelib install newgrounds
 haxelib git polymod https://github.com/larsiusprime/polymod.git
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc

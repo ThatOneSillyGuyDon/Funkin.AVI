1fe94daba120555616b12fc35920fe00ad42c812
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-09-17 15:01:21 -0500

    me when Indie Cross code

--- source/Main.hx
+++ source/Main.hx
@@ -12,6 +12,9 @@ import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 //crash handler stuff
 #if Goofy_Ahh_Crash_Thing
 import lime.app.Application;
@@ -35,6 +38,8 @@ class Main extends Sprite
 	var startFullscreen:Bool = false; // N o
 	public static var gjToastManager:GJToastManager; //Toast For Advice
 	public static var fpsVar:FPS;
+
+	public static var focusMusicTween:FlxTween;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
@@ -107,6 +112,133 @@ class Main extends Sprite
 
 		FlxG.autoPause = false;
 		FlxG.mouse.visible = false;
+
+		Application.current.window.onFocusOut.add(onWindowFocusOut);
+		Application.current.window.onFocusIn.add(onWindowFocusIn);
 	}
+
+	var game:FlxGame;
+	var oldVol:Float = 1.0;
+	var newVol:Float = 0.3;
+
+	public static var focused:Bool = true;
+
+	// funi Indie Cross volume code
+	function onWindowFocusOut()
+		{
+			focused = false;
+	
+			// Lower global volume when unfocused
+			if (Type.getClass(FlxG.state) != PlayState) // imagine stealing my code smh
+			{
+				oldVol = FlxG.sound.volume;
+				if (oldVol > 0.3)
+				{
+					newVol = 0.3;
+				}
+				else
+				{
+					if (oldVol > 0.1)
+					{
+						newVol = 0.1;
+					}
+					else
+					{
+						newVol = 0;
+					}
+				}
+	
+				trace("Game unfocused");
+	
+				if (focusMusicTween != null)
+					focusMusicTween.cancel();
+				focusMusicTween = FlxTween.tween(FlxG.sound, {volume: newVol}, 0.5);
+	
+				// Conserve power by lowering draw framerate when unfocuced
+				FlxG.drawFramerate = 60;
+			}
+		}
+	
+		function onWindowFocusIn()
+		{
+			new FlxTimer().start(0.2, function(tmr:FlxTimer)
+			{
+				focused = true;
+			});
+	
+			// Lower global volume when unfocused
+			if (Type.getClass(FlxG.state) != PlayState)
+			{
+				trace("Game focused");
+	
+				// Normal global volume when focused
+				if (focusMusicTween != null)
+					focusMusicTween.cancel();
+	
+				focusMusicTween = FlxTween.tween(FlxG.sound, {volume: oldVol}, 0.5);
+	
+				// Bring framerate back when focused
+				FlxG.drawFramerate = 120;
+			}
+		}
+
+		/*function onCrash(e:UncaughtErrorEvent):Void
+			{
+				var errMsg:String = "";
+				var path:String;
+				var callStack:Array<StackItem> = CallStack.exceptionStack(true);
+				var dateNow:String = Date.now().toString();
+		
+				dateNow = StringTools.replace(dateNow, " ", "_");
+				dateNow = StringTools.replace(dateNow, ":", "'");
+		
+				path = "./crash/" + "FunkinAVI_" + dateNow + ".txt";
+		
+				errMsg = "Version: " + Lib.application.meta["version"] + "\n";
+		
+				for (stackItem in callStack)
+				{
+					switch (stackItem)
+					{
+						case FilePos(s, file, line, column):
+							errMsg += file + " (line " + line + ")\n";
+						default:
+							Sys.println(stackItem);
+					}
+				}
+		
+				errMsg += "\nUncaught Error: " + e.error + "\nReport the error here: https://discord.gg/cZydhxFYpp";
+		
+				if (!FileSystem.exists("./crash/"))
+					FileSystem.createDirectory("./crash/");
+		
+				File.saveContent(path, errMsg + "\n");
+		
+				Sys.println(errMsg);
+				Sys.println("Crash dump saved in " + Path.normalize(path));
+		
+				var crashDialoguePath:String = "FlixelCrashHandler";
+		
+				#if windows
+				crashDialoguePath += ".exe";
+				#end
+		
+				if (FileSystem.exists("./" + crashDialoguePath))
+				{
+					Sys.println("Found crash dialog: " + crashDialoguePath);
+		
+					#if linux
+					crashDialoguePath = "./" + crashDialoguePath;
+					#end
+					new Process(crashDialoguePath, [path]);
+				}
+				else
+				{
+					Sys.println("No crash dialog found! Making a simple alert instead...");
+					Application.current.window.alert(errMsg, "Error!");
+				}
+		
+				Sys.exit(1);
+			}*/
 }
 	

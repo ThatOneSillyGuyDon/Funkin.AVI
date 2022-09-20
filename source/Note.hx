0fc0b313390aef4a73be1d9ec060af29657dc322
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2022-09-20 10:48:17 -0500

    forgot to hardcode note skin onto Mickey's legacy json

--- source/Note.hx
+++ source/Note.hx
@@ -248,7 +248,7 @@ class Note extends FlxSprite
 			case 'spooky':
 				skin = 'Skins/SpookyNOTE_assets';
 				antialias = true;
-			case 'mickey' | 'mickeysadistic' | 'mickeyinsane' | 'mickeygiveup' | 'mickeynew' | 'bf-fake' | 'bffake' | 'bf-demon' | 'bfgrey' | 'bfsatan' | 'bfworried' | 'huntergoofy' | 'smiles':
+			case 'mickey' | 'mickeysadistic' | 'mickeyinsane' | 'mickeylegacy' | 'mickeygiveup' | 'mickeynew' | 'bf-fake' | 'bffake' | 'bf-demon' | 'bfgrey' | 'bfsatan' | 'bfworried' | 'huntergoofy' | 'smiles':
 				skin = 'Skins/GreyNOTE_assets';
 				antialias = true;
 			case 'walt' | 'bf-firstperson':

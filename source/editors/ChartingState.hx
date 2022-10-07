7ce6dec32380937566a9c001da990e4f5d77f3bd
Jsa010 <98182734+Jsa010@users.noreply.github.com>
2022-10-07 16:24:26 -0300

    why this dont even appear

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -1053,17 +1053,17 @@ class ChartingState extends MusicBeatState
 
 		var text:FlxText = new FlxText(20, 90, 0, "Value 1:");
 		tab_group_event.add(text);
-		value1InputText = new FlxUIInputText(20, 90, 100, "");
+		value1InputText = new FlxUIInputText(20, 110, 100, "");
 		blockPressWhileTypingOn.push(value1InputText);
 
 		var text:FlxText = new FlxText(20, 130, 0, "Value 2:");
 		tab_group_event.add(text);
-		value2InputText = new FlxUIInputText(20, 130, 100, "");
+		value2InputText = new FlxUIInputText(20, 150, 100, "");
 		blockPressWhileTypingOn.push(value2InputText);
 
 		var text:FlxText = new FlxText(20, 170, 0, "Value 3:");
 		tab_group_event.add(text);
-		value3InputText = new FlxUIInputText(20, 170, 100, "");
+		value3InputText = new FlxUIInputText(20, 190, 100, "");
 		blockPressWhileTypingOn.push(value3InputText);
 
 		// New event buttons
